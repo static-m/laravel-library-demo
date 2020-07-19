1bd678a38128276647fcd74f945ae8c77102dc22
Martin Gazdag <gazdag.martin@gmail.com>
2020-07-19 17:46:15 +0200

    Added form for adding a book

--- resources/views/books/add.blade.php
+++ resources/views/books/add.blade.php
@@ -5,5 +5,28 @@
 @section('sidebar')
 
 @section('content')
-    <p>Pridať knihu</p>
+    <div class="container">
+        <h1>Pridať knihu</h1>
+
+        {!!Form::open()!!}
+
+        {!!Form::text('name', 'Názov')->required()->placeholder('Názov knihy')->lg()!!}
+
+        {!!Form::text('isbn', 'ISBN')->placeholder('ISBN')->lg()!!}
+
+        {!!Form::text('genre', 'Žáner')->placeholder('Žáner')->lg()!!}
+
+        {!!Form::textarea('abstract', 'Abstrakt')->placeholder('Abstrakt...')->lg()!!}
+
+        {!!Form::date('published_date', 'Publikované dňa')->lg()!!}
+        {!!Form::time('published_time', 'Publikované o hodine')->lg()!!}
+
+        {!!Form::text('author_email', 'E-mail autora')->type('email')->placeholder('E-mail autora')->lg()!!}
+
+        {!!Form::range('pages', 'Počet strán')->type('number')->placeholder('Počet strán')->lg()!!}
+
+        {!!Form::submit("Pridať")->lg()!!}
+
+        {!!Form::close()!!}
+    </div>
 @endsection

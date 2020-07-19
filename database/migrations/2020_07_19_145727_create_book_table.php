4e9f0f25d43d3ccdefed1ea34943dc5361dffc7a
Martin Gazdag <gazdag.martin@gmail.com>
2020-07-19 17:22:22 +0200

    Added migration for the book table

---
+++ database/migrations/2020_07_19_145727_create_book_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('isbn')->nullable();
+            $table->string('genre')->nullable();
+            $table->text('abstract')->nullable();
+            $table->dateTime('published_on')->nullable();
+            $table->string('author_email')->nullable();
+            $table->integer('pages')->nullable();
+            $table->timestamps();
+
+            $table->index('name');
+            $table->index('published_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
